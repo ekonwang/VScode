3ba0f3a94bdf4a3d05688cd68aa212f7b10e78fc
ekon <1547246193@qq.com>
2021-04-05 20:09:35 +0800

    train atcoder c/d

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+#define PI acos(-1)
+
+int main() {
+    double t;
+    cout << setprecision(10) << (t = atan2(0, -1)) << endl;
+    cout << PI << endl;
+    return 0;
+}
