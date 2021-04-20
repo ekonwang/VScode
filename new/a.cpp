436ac7bf963dd437bc691ace37b239ba64e2f2e7
ekon <1547246193@qq.com>
2021-04-20 17:10:49 +0800

    codeforce commits

--- new/a.cpp
+++ new/a.cpp
@@ -1,68 +1,85 @@
-#include <bits/stdc++.h>                                                     //*
-#define il inline                                                            //*
-#define ll long long                                                         //*
-#define INT __int64                                                          //*
-#define usn unsigned                                                         //*
-#define FOR(i,a,b) for(int i = (a), (i##i)=(b); i<=(i##i); ++i)              //*
-#define REP(i,a,b) for(int i = (a), (i##i)=(b); i>=(i##i); --i)              //*
-ll const MAXLL = 0x7fffffffffffffffLL;                                       //*
-int const MAXINT = 0x7fffffff;                                               //*
-const int INF = 0x3f3f3f3f;                                                  //*
-const double E = exp(1.0);                                                   //*
-const double PI = acos(-1.0);                                                //*
-ll gcd(ll a,ll b){while(b^=a^=b^=a%=b);return a;}                            //*
-ll lcd(ll a , ll b){return a * b / gcd(a,b);}                                //*
-inline ll read(){                                                            //*
-    char ch = getchar(); ll x = 0, f = 1;                                    //*
-    while(ch < '0' || ch > '9') {if(ch == '-') f = -1; ch = getchar();}      //*
-    while('0' <= ch && ch <= '9') {x = x * 10 + ch - '0'; ch = getchar();}   //*
-    return x * f;                                                            //*
-}                                                                            //*
-inline void write(ll x)                                                      //*
-{                                                                            //*
-    if(x<0) {                                                                //*
-        putchar('-');                                                        //*
-        x = -x;                                                              //*
-    }                                                                        //*
-    if(x>9) write(x / 10);                                                   //*
-    putchar(x % 10 + '0');                                                   //*
-}                                                                            //*
-void init_cin(){                                                             //*
-  std::ios::sync_with_stdio(false);                                          //*
-  std::cin.tie(nullptr);                                                     //*
-  std::cout.tie(nullptr);                                                    //*
-}                                                                            //*
-using namespace std;                                                         //*
+    #include <bits/stdc++.h>                                                     //*
+    #define il inline                                                            //*
+    #define ll long long                                                         //*
+    #define INT __int64                                                          //*
+    #define usn unsigned                                                         //*
+    #define FOR(i,a,b) for(int i = (a), (i##i)=(b); i<=(i##i); ++i)              //*
+    #define REP(i,a,b) for(int i = (a), (i##i)=(b); i>=(i##i); --i)              //*
+    ll const MAXLL = 0x7fffffffffffffffLL;                                       //*
+    int const MAXINT = 0x7fffffff;                                               //*
+    const int INF = 0x3f3f3f3f;                                                  //*
+    const double E = exp(1.0);                                                   //*
+    const double PI = acos(-1.0);                                                //*
+    ll gcd(ll a,ll b){while(b^=a^=b^=a%=b);return a;}                            //*
+    ll lcd(ll a , ll b){return a * b / gcd(a,b);}                                //*
+    ll phi(ll a) {                                                               //*
+        ll tmp = a, ans = a, d = 2;                                              //*
+        while(d * d <= tmp) {                                                    //*
+            ll cnt = 0;                                                          //*
+            while(tmp % d == 0) {tmp /= d; cnt ++;} if(cnt) ans -= ans / d; d++; //*
+        }                                                                        //*
+        if(tmp > 1) ans -= ans/tmp;                                              //*
+        return ans;                                                              //*
+    }
+    inline ll read(){                                                            //*
+        char ch = getchar(); ll x = 0, f = 1;                                    //*
+        while(ch < '0' || ch > '9') {if(ch == '-') f = -1; ch = getchar();}      //*
+        while('0' <= ch && ch <= '9') {x = x * 10 + ch - '0'; ch = getchar();}   //*
+        return x * f;                                                            //*
+    }                                                                            //*
+    inline void write(ll x)                                                      //*
+    {                                                                            //*
+        if(x<0) {                                                                //*
+            putchar('-');                                                        //*
+            x = -x;                                                              //*
+        }                                                                        //*
+        if(x>9) write(x / 10);                                                   //*
+        putchar(x % 10 + '0');                                                   //*
+    }                                                                            //*
+    void init_cin(){                                                             //*
+    std::ios::sync_with_stdio(false);                                            //*
+    std::cin.tie(nullptr);                                                       //*
+    std::cout.tie(nullptr);                                                      //*
+    }                                                                            //*
+    using namespace std;                                                         //*
 
-int cmp(pair<int,int>a,pair<int,int>b){
-    return a.first < b.first || (a.first == b.first && a.second < b.second);
+    int cmp(pair<int,int>a,pair<int,int>b){
+        return a.first < b.first || (a.first == b.first && a.second < b.second);
+    }
+    void clear(queue<int>& q) {
+        queue<int> empty;
+        swap(empty, q);
+    }
+    //---------------------------------------------------------------------------//*
+
+
+
+const int N = 100 + 5;
+set<int> squares;
+int a[N];
+
+void pre() {
+    FOR(i, 1, 100)
+        squares.insert(i * i);
 }
-void clear(queue<int>& q) {
-	queue<int> empty;
-	swap(empty, q);
-}
-//---------------------------------------------------------------------------//*
-
-
-
-const int N = 100000 + 5;
-int t, n, x1, x2, x3, v;
 
 void solve() {
+    int t, n;
     cin >> t;
+    pre();
     while(t--) {
+        int f = 0;
         cin >> n;
+        FOR(i, 1, n) cin >> a[i];
         FOR(i, 1, n) {
-            cin >> v;
-            if (i == n) x3 = v;
-            else if(i == 1) x1 = v;
-            else if(i == 2) x2 = v;
+            if(squares.count(a[i]) == 0) {
+                f = 1;
+                break;
+            }
+            //cout << "i a[i]" << i << ' ' << a[i] << endl;
         }
-
-        if(x3 >= (x1 + x2)) {
-            cout << 1 << ' ' << 2 << ' ' << n << endl;
-        }
-        else cout << -1 << endl;
+        if(!f) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
 }
 
