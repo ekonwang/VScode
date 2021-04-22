c7657a343aab80b778eb0fd2e9ab21fad8610036
ekonwang <1547246193@qq.com>
2021-04-22 19:08:48 +0800

    template changes

--- contest/template.cpp
+++ contest/template.cpp
@@ -1,50 +1,38 @@
-    #include <bits/stdc++.h>                                                     //*
-    #define il inline                                                            //*
-    #define ll long long                                                         //*
-    #define INT __int64                                                          //*
-    #define usn unsigned                                                         //*
-    #define FOR(i,a,b) for(int i = (a), (i##i)=(b); i<=(i##i); ++i)              //*
-    #define REP(i,a,b) for(int i = (a), (i##i)=(b); i>=(i##i); --i)              //*
-    ll const MAXLL = 0x7fffffffffffffffLL;                                       //*
-    int const MAXINT = 0x7fffffff;                                               //*
-    const int INF = 0x3f3f3f3f;                                                  //*
-    const double E = exp(1.0);                                                   //*
-    const double PI = acos(-1.0);                                                //*
-    ll gcd(ll a,ll b){while(b^=a^=b^=a%=b);return a;}                            //*
-    ll lcd(ll a , ll b){return a * b / gcd(a,b);}                                //*
-    ll phi(ll a) {                                                               //*
-        ll tmp = a, ans = a, d = 2;                                              //*
-        while(d * d <= tmp) {                                                    //*
-            ll cnt = 0;                                                          //*
-            while(tmp % d == 0) {tmp /= d; cnt ++;} if(cnt) ans -= ans / d; d++; //*
-        }                                                                        //*
-        if(tmp > 1) ans -= ans/tmp;                                              //*
-        return ans;                                                              //*
-    }
-    inline ll read(){                                                            //*
-        char ch = getchar(); ll x = 0, f = 1;                                    //*
-        while(ch < '0' || ch > '9') {if(ch == '-') f = -1; ch = getchar();}      //*
-        while('0' <= ch && ch <= '9') {x = x * 10 + ch - '0'; ch = getchar();}   //*
-        return x * f;                                                            //*
-    }                                                                            //*
-    void init_cin(){                                                             //*
-    std::ios::sync_with_stdio(false);                                            //*
-    std::cin.tie(nullptr);                                                       //*
-    std::cout.tie(nullptr);                                                      //*
-    }                                                                            //*
-    using namespace std;                                                         //*
-
-    int cmp(pair<int,int>a,pair<int,int>b){
-        return a.first < b.first || (a.first == b.first && a.second < b.second);
-    }
-    //---------------------------------------------------------------------------//*
-
-
+#include <bits/stdc++.h>                                                     //*
+#define FOR(i,a,b) for(int (i) = (a); i <= (b); ++i)                         //*
+#define REP(i,a,b) for(int (i) = (a); i >= (b); --i)                         //*
+using namespace std;                                                         //*
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef struct{int lchild, rchild, index; } node;
+const ll MAXLL = 0x7fffffffffffffffLL;                                       //*
+const int MAXINT = 0x7fffffff;                                               //*
+const int INF = 0x3f3f3f3f;                                                  //*
+const double E = exp(1.0);                                                   //*
+const double PI = acos(-1.0);                                                //*
+ll gcd(ll a,ll b){while(b^=a^=b^=a%=b);return a;}                            //*
+ll lcd(ll a , ll b){return a * b / gcd(a,b);}                                //*
+ll phi(ll a) {                                                               //*
+    ll tmp = a, ans = a, d = 2;                                              //*
+    while(d * d <= tmp) {                                                    //*
+        ll cnt = 0;                                                          //*
+        while(tmp % d == 0) {tmp /= d; cnt ++;} if(cnt) ans -= ans / d; d++; //*
+    }                                                                        //*
+    if(tmp > 1) ans -= ans/tmp;                                              //*
+    return ans;                                                              //*
+}
+void init_cin(){                                                             //*
+std::ios::sync_with_stdio(false);                                            //*
+std::cin.tie(nullptr);                                                       //*
+std::cout.tie(nullptr);                                                      //*
+}                                                                            //*
+//---------------------------------------------------------------------------//*
 
 const int N = 100000 + 5;
 
 void solve() {
-    
+
 }
 
 int main() {
